c4ac52bad4b2eaa04b34a5fc4c623de7dd35ceb5
matthew487-hub <kwokmatthew097@gmail.com>
2022-03-08 12:37:32 +0800

    weird thing

--- src/App.js
+++ src/App.js
@@ -70,6 +70,61 @@ function App() {
   const onMapLoad = React.useCallback((map) => {
     mapRef.current = map;
   }, []);
+
+  const getBtn = document.getElementById("get-btn");
+  const postBtn = document.getElementById("post-btn");
+
+  const sendHttpRequest = (method, url, data) => {
+    const promise = new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open(method, url);
+
+      xhr.responseType = "json";
+
+      if (data) {
+        xhr.setRequestHeader("Content-Type", "application/json");
+      }
+
+      xhr.onload = () => {
+        if (xhr.status >= 400) {
+          reject(xhr.response);
+        } else {
+          resolve(xhr.response);
+        }
+      };
+
+      xhr.onerror = () => {
+        reject("Something went wrong!");
+      };
+
+      xhr.send(JSON.stringify(data));
+    });
+    return promise;
+  };
+
+  const getData = () => {
+    sendHttpRequest("GET", "http://127.0.0.1:5000/").then((responseData) => {
+      console.log(responseData);
+    });
+  };
+
+  const sendData = () => {
+    sendHttpRequest("POST", "http://127.0.0.1:5000/", {
+      wcost: "0.5",
+      pt: "shatin,lokfu",
+      cost: "2000",
+    })
+      .then((responseData) => {
+        console.log(responseData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  getBtn.addEventListener("click", getData);
+  postBtn.addEventListener("click", sendData);
+
   const [markers, setMarkers] = React.useState([]);
 
   const [stops, setStops] = useState([null, null]);
@@ -109,6 +164,8 @@ function App() {
           >
             Add stops
           </Button>
+          <button id="get-btn">Get Data</button>
+          <button id="post-btn">post Data</button>
           <div className="goal">
             <div className="goal-title">
               <Box bg="grey" w="100%" p={0.2} color="white">
